0d43d52a80fc4a7e9c0b7fb056c4d456413b2ecc
Richard Hildred <rhildred@gmail.com>
2024-08-25 00:19:04 +0000

    builds

--- build.js
+++ build.js
@@ -1,25 +1,14 @@
 import fs from 'node:fs';
 import { exec } from 'node:child_process';
 
-function run(cmd) {
-    return new Promise((resolve, reject) => {
-      exec(cmd, (error, stdout, stderr) => {
-        if (error) return reject(error)
-        if (stderr) return reject(stderr)
-        resolve(stdout)
-      })
-    })
-  }
-  
-
 const folderPath = '.';
 
 const aFolders = await fs.promises.readdir(folderPath);
 
 for(const sFolder of aFolders){
     const oStats = await fs.promises.lstat(sFolder);
-    if(!['.git', 'node_modules', '.github'].includes(sFolder) && oStats.isDirectory()){
-        console.log(await run(`R -e 'rmarkdown::render("${sFolder}/slides.Rmd", output_format = "powerpoint_presentation", output_dir = "dist/${sFolder}")'`));
+    if(!['.git', 'node_modules', '.github', '.devcontainer'].includes(sFolder) && oStats.isDirectory()){
+        exec(`R -e 'rmarkdown::render("${sFolder}/slides.Rmd", output_format = "powerpoint_presentation", output_dir = "dist/${sFolder}")'`);
     }
 }
 
