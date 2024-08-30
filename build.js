9997cd3ccc68aa13db90d7fe42cf923c81b8c828
rhildred <rhildred@gmail.com>
2024-08-30 17:12:51 +0000

    generate html as well with rMarkdown

--- build.js
+++ build.js
@@ -9,6 +9,7 @@ for(const sFolder of aFolders){
     const oStats = await fs.promises.lstat(sFolder);
     if(!['.git', 'node_modules', '.github', '.devcontainer'].includes(sFolder) && oStats.isDirectory()){
         exec(`R -e 'rmarkdown::render("${sFolder}/slides.Rmd", output_format = "powerpoint_presentation", output_dir = "dist/${sFolder}")'`);
+        exec(`R -e 'rmarkdown::render("${sFolder}/slides.Rmd", output_format = "html_document", output_dir = "dist/${sFolder}")'`);
     }
 }
 
