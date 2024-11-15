55a462af3f1612ba30b91d20da866acf9ccb3313
rhildred <rhildred@gmail.com>
2024-11-15 21:03:47 +0000

    more material

--- build.js
+++ build.js
@@ -8,8 +8,8 @@ const aFolders = await fs.promises.readdir(folderPath);
 for(const sFolder of aFolders){
     const oStats = await fs.promises.lstat(sFolder);
     if(!['dist','.git', 'node_modules', '.github', '.devcontainer'].includes(sFolder) && oStats.isDirectory()){
-        exec(`R -e 'rmarkdown::render("${sFolder}/slides.Rmd", output_format = "powerpoint_presentation", output_dir = "dist/${sFolder}")'`);
         exec(`R -e 'rmarkdown::render("${sFolder}/slides.Rmd", output_format = "html_document", output_dir = "dist/${sFolder}")'`);
+        exec(`R -e 'rmarkdown::render("${sFolder}/slides.Rmd", output_format = "powerpoint_presentation", output_dir = "dist/${sFolder}")'`);
     }
 }
 
