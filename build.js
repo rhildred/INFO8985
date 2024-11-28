edecbfb134b5516a109acfa0dedc9d2250007c77
rhildred <rhildred@gmail.com>
2024-11-28 17:32:14 +0000

    error handling

--- build.js
+++ build.js
@@ -1,5 +1,6 @@
 import fs from 'node:fs';
 import { exec } from 'node:child_process';
+import { errorMonitor } from 'node:events';
 
 const folderPath = '.';
 
@@ -8,8 +9,17 @@ const aFolders = await fs.promises.readdir(folderPath);
 for(const sFolder of aFolders){
     const oStats = await fs.promises.lstat(sFolder);
     if(!['dist','.git', 'node_modules', '.github', '.devcontainer'].includes(sFolder) && oStats.isDirectory()){
-        await exec(`R -e 'rmarkdown::render("${sFolder}/slides.Rmd", output_format = "html_document", output_dir = "dist/${sFolder}")'`);
-        await exec(`R -e 'rmarkdown::render("${sFolder}/slides.Rmd", output_format = "powerpoint_presentation", output_dir = "dist/${sFolder}")'`);
+        exec(`R -e 'rmarkdown::render("${sFolder}/slides.Rmd", output_format = "html_document", output_dir = "dist/${sFolder}")'`, {}, (error, stdout, stderr)=>{
+            if(error){
+                throw `${error}, ${stdout}, ${stderr}`;
+            }
+            exec(`R -e 'rmarkdown::render("${sFolder}/slides.Rmd", output_format = "powerpoint_presentation", output_dir = "dist/${sFolder}")'`, {}, (error, stdout, stderr)=>{
+                if(error){
+                    throw `${error}, ${stdout}, ${stderr}`;
+                }
+
+            });
+        });
     }
 }
 
